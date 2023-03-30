cd9da8e7f60ed228e90a5c21cfcf0413fd78c399
Dhaiwat / dhai.eth <dhaiwatpandya@gmail.com>
2023-03-30 12:55:51 +0530

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Provider from 'streamr-client-react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider
+      auth={{
+        privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY as string, // BAD, DONT USE IN PROD
+      }}
+    >
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
